c288d573e28e2fd12865630d4932552576965568
赵宸兴宇 <zhaochenxingyu@zhaochenxingyudeMacBook-Pro.local>
2018-07-12 18:30:55 +0800

    task

--- cloud_upload_download.py
+++ cloud_upload_download.py
@@ -15,27 +15,12 @@ config = CosConfig(Secret_id=secret_id, Secret_key=secret_key, Region=region, To
 #获取客户端对象
 client = CosS3Client(config)
 
-#文件上传，速度大约100M/min
-#待上传文件的本地路径
-input_path = './zipf/030.dat'
-#文件上传后在云盘存储桶中的路径
-upload_path = './zipf/030.dat'
-#start_time = time.time()
-with open(input_path, 'rb') as fp:
-    response = client.put_object(
-        Bucket = 'test01-1257045059',
-        Body = fp,
-        Key = upload_path,
-    )
-print(response['ETag'])
-#end_time = time.time()
-#print(end_time - start_time)
 
 #文件下载
 #文件下载后的本地路径
-output_path = './cloudTest/formatted00.dat'
+output_path = './kosarak.dat'
 #待下载文件在云盘存储桶中的路径
-download_path = 'formatted00.dat'
+download_path = 'kosarak.dat'
 #start_time = time.time()
 response = client.get_object(
     Bucket = 'test01-1257045059',
